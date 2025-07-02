b5314c669702dff66950da6a2147cf5f0b574b2d
Marisabel <aramayomarisabel@gmail.com>
2025-07-02 16:02:10 -0300

    Agregado nombre profesor

--- integrador.py
+++ integrador.py
@@ -2,7 +2,7 @@
 # Análisis de Algoritmos en Python
 # Título: La eficacia de los algoritmos y su impacto en el rendimiento de sistemas
 # Alumno: Marisabel Viviana Aramayo
-# Profesor: —
+# Profesor: Alberto Cortez
 # Fecha de entrega: 4 de julio de 2025
 
 import time
